9b536e8b673347ad088e3286304a4d6e25a68d3f
kc1212 <kc1212@users.noreply.github.com>
2023-01-23 21:31:38 +0100

    Impl sort

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,24 @@ where
         Self { vs }
     }
 
-    pub fn merge(&mut self, lo: usize, n: usize, r: usize) {
+    pub fn sort(&mut self) {
+        self.sort_rec(0, self.vs.len());
+    }
+
+    fn sort_rec(&mut self, lo: usize, n: usize) {
+        if n > 1 {
+            let m = n/2;
+            self.sort_rec(lo, m);
+            self.sort_rec(lo + m, m);
+            self.merge_rec(lo, n, 1);
+        }
+    }
+
+    pub fn merge_rec(&mut self, lo: usize, n: usize, r: usize) {
         let m = r * 2;
         if m < n {
-            self.merge(lo, n, m);
-            self.merge(lo + r, n, m);
+            self.merge_rec(lo, n, m);
+            self.merge_rec(lo + r, n, m);
             for i in (lo + r..lo + n - r).step_by(m) {
                 compare_at(&mut self.vs, i, i + r);
             }
@@ -43,21 +56,28 @@ mod test {
     #[test]
     fn test_merge_2() {
         let mut batcher = BatcherSort::new(vec![2, 1]);
-        batcher.merge(0, 2, 1);
+        batcher.merge_rec(0, 2, 1);
         assert_eq!(vec![1, 2], batcher.vs);
     }
 
     #[test]
     fn test_merge_4() {
         let mut batcher = BatcherSort::new(vec![1, 5, 2, 4]);
-        batcher.merge(0, 4, 1);
+        batcher.merge_rec(0, 4, 1);
         assert_eq!(vec![1, 2, 4, 5], batcher.vs);
     }
 
     #[test]
     fn test_merge_8() {
         let mut batcher = BatcherSort::new(vec![1, 5, 6, 7, 2, 3, 4, 5]);
-        batcher.merge(0, 8, 1);
+        batcher.merge_rec(0, 8, 1);
+        assert_eq!(vec![1, 2, 3, 4, 5, 5, 6, 7], batcher.vs);
+    }
+
+    #[test]
+    fn test_sort() {
+        let mut batcher = BatcherSort::new(vec![1, 5, 6, 7, 2, 4, 3, 5]);
+        batcher.sort();
         assert_eq!(vec![1, 2, 3, 4, 5, 5, 6, 7], batcher.vs);
     }
 }
