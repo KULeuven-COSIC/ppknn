38cc171eedd42a56ec56fd84b5b545efb8d7da60
kc1212 <kc1212@users.noreply.github.com>
2023-05-15 13:29:51 +0200

    Take k as input in bench

--- bench.sh
+++ bench.sh
@@ -3,10 +3,16 @@
 set -e
 set -u
 
+# USAGE:
+# ./bench 40 3 5
+# first argument is the model size, the remaining are different values of k
+_CHECK=$2 # check that there is at least one value of k
+
 FILENAME=data/mnist-8x8.csv
 MODELSIZE=$1
+KS="${@:2}" # https://stackoverflow.com/questions/2701400/remove-first-element-from-in-bash
 
-for k in 3 5 7; do
+for k in $KS; do
     cargo run --release -- \
         --file-name "$FILENAME" \
         --model-size "$MODELSIZE" \
@@ -14,5 +20,6 @@ for k in 3 5 7; do
         --repetitions 10 \
         -k "$k" \
         --initial-modulus 64  \
-        --binary-threshold 8
+        --binary-threshold 8 \
+        --csv
 done
