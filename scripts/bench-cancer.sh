43566d8f21d66f43f03e794bc9a14e9581a81f04
kc1212 <kc1212@users.noreply.github.com>
2023-05-17 15:29:49 +0200

    Add cancer dataset and script

---
+++ scripts/bench-cancer.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+# USAGE, e.g.,
+# ./bench-cancer 10 3 5
+# first argument is the model size, the remaining are different values of k
+
+FILENAME=data/cancer.csv
+MODELSIZE=$1
+KS="${@:2}" # https://stackoverflow.com/questions/2701400/remove-first-element-from-in-bash
+
+_CHECK=$2 # check that there is at least one value of k
+
+
+cargo build --release
+
+INITIAL_MODULUS=128
+for K in $KS; do
+    ./target/release/ppknn \
+        --file-name "$FILENAME" \
+        --model-size "$MODELSIZE" \
+        --test-size 100 \
+        --repetitions 10 \
+        -k "$K" \
+        --initial-modulus "$INITIAL_MODULUS" \
+        --quantize-type ternary \
+        --csv
+done
