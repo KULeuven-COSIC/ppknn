2c5894bfeb680cd748f346f388f1c36aff9f6172
kc1212 <kc1212@users.noreply.github.com>
2023-05-22 16:36:40 +0200

    Add ks benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,4 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use ppknn::setup_with_modulus;
 use tfhe::shortint::prelude::*;
 
 const PARAMS: Parameters = Parameters {
@@ -9,8 +10,8 @@ const PARAMS: Parameters = Parameters {
 
 pub fn pbs_benchmark(c: &mut Criterion) {
     let (client, server) = gen_keys(PARAMS);
-
     let ct = client.encrypt(1);
+
     c.bench_function("pbs", |b| {
         b.iter(|| {
             let _ = server.keyswitch_bootstrap(&ct);
@@ -18,5 +19,17 @@ pub fn pbs_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, pbs_benchmark,);
+pub fn ks_benchmark(c: &mut Criterion) {
+    let dist_mod = PARAMS.message_modulus.0 * 2;
+    let (client, server) = setup_with_modulus(PARAMS, dist_mod as u64);
+    let ct = client.key.encrypt(1);
+
+    c.bench_function("ks", |b| {
+        b.iter(|| {
+            let _ = server.lwe_to_glwe(&ct);
+        });
+    });
+}
+
+criterion_group!(benches, pbs_benchmark, ks_benchmark);
 criterion_main!(benches);
