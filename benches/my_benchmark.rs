efbb32a6befeeba90027171f806d4c83b587915c
kc1212 <kc1212@users.noreply.github.com>
2023-10-23 16:15:40 +0200

    Add encrypted network bench

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,5 +1,9 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex, RwLock};
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use ppknn::server::setup_with_data;
+use ppknn::{network::*, AsyncEncComparator, EncItem};
 use tfhe::shortint::prelude::*;
 
 const PARAMS: Parameters = Parameters {
@@ -8,7 +12,7 @@ const PARAMS: Parameters = Parameters {
     ..PARAM_MESSAGE_2_CARRY_3
 };
 
-pub fn pbs_benchmark(c: &mut Criterion) {
+fn pbs_benchmark(c: &mut Criterion) {
     let (client, server) = gen_keys(PARAMS);
     let ct = client.encrypt(1);
 
@@ -19,7 +23,7 @@ pub fn pbs_benchmark(c: &mut Criterion) {
     });
 }
 
-pub fn ks_benchmark(c: &mut Criterion) {
+fn ks_benchmark(c: &mut Criterion) {
     let dist_mod = PARAMS.message_modulus.0 * 2;
     let (client, server) = setup_with_data(PARAMS, &vec![], &vec![], dist_mod as u64);
     let ct = client.key.encrypt(1);
@@ -31,5 +35,45 @@ pub fn ks_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, pbs_benchmark, ks_benchmark);
+fn network_bench(c: &mut Criterion) {
+    let d = 20usize;
+    let k = 3usize;
+    let pb: PathBuf = [
+        env!("CARGO_MANIFEST_DIR"),
+        "data",
+        &format!("network-{}-{}.csv", d, k),
+    ]
+    .iter()
+    .collect();
+    let network = load_network(pb.as_path()).unwrap();
+
+    let dist_mod = PARAMS.message_modulus.0 * 2;
+    // data and labels not actually used if we just need to use the comparator
+    let (mut client, server) = setup_with_data(PARAMS, &vec![], &vec![], dist_mod as u64);
+    let server = Arc::new(RwLock::new(server));
+    let cmp = AsyncEncComparator::new(server, PARAMS);
+
+    // just create dummy elements
+    let actual = (0..d).map(|_| {
+        EncItem::new(
+            client.lwe_encrypt_with_delta(0, 0),
+            client.lwe_encrypt_with_delta(0, 0),
+        )
+    });
+    let a_actual: Vec<_> = actual.map(|x| Arc::new(Mutex::new(x))).collect();
+
+    for n_threads in [1, 2].iter() {
+        c.bench_with_input(
+            BenchmarkId::new("enc network", n_threads),
+            &n_threads,
+            |b, &t| {
+                b.iter(|| {
+                    do_work(*t, &network, cmp.clone(), &a_actual);
+                });
+            },
+        );
+    }
+}
+
+criterion_group!(benches, pbs_benchmark, ks_benchmark, network_bench);
 criterion_main!(benches);
